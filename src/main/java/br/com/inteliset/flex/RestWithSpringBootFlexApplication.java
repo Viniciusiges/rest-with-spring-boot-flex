ebbb4f4e07e92b8f5b8c1bff9d5a67c49f72ce83
viniciusiges <viniciusiges@gmail.com>
2023-12-28 22:38:11 -0300

    Adicionando projeto

---
+++ src/main/java/br/com/inteliset/flex/RestWithSpringBootFlexApplication.java
@@ -0,0 +1,13 @@
+package br.com.inteliset.flex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestWithSpringBootFlexApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestWithSpringBootFlexApplication.class, args);
+    }
+
+}
